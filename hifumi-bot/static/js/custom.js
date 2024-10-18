31276bbbaf9619acdc0b42c85a69a529f06ec5da
wstudio-group <wangrun114514@gmail.com>
2024-10-18 22:00:13 +0800

    first commit

---
+++ hifumi-bot/static/js/custom.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?dc96f70654d2b88928f758abce6e382d";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+("#Background").height($("#Background").height());
